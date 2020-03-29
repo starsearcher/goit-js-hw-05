afca67932db8a99c352f05c60b81f1a3aee52449
starsearcher <57115514+starsearcher@users.noreply.github.com>
2020-03-30 02:39:30 +0300

    v 1.0

---
+++ task-05.js
@@ -0,0 +1,161 @@
+class Car {
+    static getSpecs(car) {
+        console.log(`maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`);
+    }
+
+    constructor({ maxSpeed, speed = 0, isOn = false, distance = 0, price }) {
+        this.maxSpeed = maxSpeed;
+        this.speed = speed;
+        this.isOn = isOn;
+        this.distance = distance;
+        this._price = price;
+    }
+
+    get price() {
+        return this._price;
+    }
+
+    set price(value) {
+        this._price = value;
+    }
+
+    turnOn() {
+        this.isOn = true;
+    }
+
+    turnOff() {
+        this.isOn = false;
+        this.speed = 0;
+    }
+
+    accelerate(value) {
+        if (this.isOn) {
+            if (this.speed + value > this.maxSpeed) {
+                this.speed = this.maxSpeed;
+                console.log(`You've reached max speed!`);
+            } else {
+                this.speed += value;
+            }
+        } else {
+            console.log('Please, turn on the car!');
+        }
+    }
+
+    decelerate(value) {
+        if (this.speed - value <= 0) {
+            this.speed = 0;
+            console.log(`The car has stopped`);
+        } else if (value < 0) {
+            value = 0;
+            console.log('Decelerator must be a positive value');
+        } else {
+            this.speed -= value;
+        }
+    }
+
+    drive(hours) {
+        if ((this.isOn = true)) {
+            this.distance += hours * this.speed;
+        } else console.log('Please, turn on the car!');
+        return this.distance;
+    }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+Car.getSpecs(mustang);
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
+
+
+// Напиши класс Car с указанными свойствами и методами.
+
+// class Car {
+//     /*
+//      * Добавь статический метод `getSpecs(car)`,
+//      * который принимает объект-машину как параметр и выводит
+//      * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
+//      */
+
+//     /*
+//      * Конструктор получает объект настроек.
+//      *
+//      * Добавь свойства будущеего экземпляра класса:
+//      *  speed - текущая скорость, изначально 0
+//      *  price - цена автомобиля
+//      *  maxSpeed - максимальная скорость
+//      *  isOn - заведен ли автомобиль, значения true или false. Изначально false
+//      *  distance - общий киллометраж, изначально 0
+//      */
+//     constructor() {}
+
+//     /*
+//      * Добавь геттер и сеттер для свойства price,
+//      * который будет работать с свойством цены автомобиля.
+//      */
+
+//     /*
+//      * Добавь код для того чтобы завести автомобиль
+//      * Записывает в свойство isOn значение true
+//      */
+//     turnOn() {}
+
+//     /*
+//      * Добавь код для того чтобы заглушить автомобиль
+//      * Записывает в свойство isOn значение false,
+//      * и сбрасывает текущую скорость в 0
+//      */
+//     turnOff() {}
+
+//     /*
+//      * Добавялет к свойству speed полученное значение,
+//      * при условии что результирующая скорость
+//      * не больше чем значение свойства maxSpeed
+//      */
+//     accelerate(value) {}
+
+//     /*
+//      * Отнимает от свойства speed полученное значение,
+//      * при условии что результирующая скорость не меньше нуля
+//      */
+//     decelerate(value) {}
+
+//     /*
+//      * Добавляет в поле distance киллометраж (hours * speed),
+//      * но только в том случае если машина заведена!
+//      */
+//     drive(hours) {}
+// }
+
+// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+// mustang.turnOn();
+// mustang.accelerate(50);
+// mustang.drive(2);
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+// mustang.decelerate(20);
+// mustang.drive(1);
+// mustang.turnOff();
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+// console.log(mustang.price); // 2000
+// mustang.price = 4000;
+// console.log(mustang.price); // 4000
